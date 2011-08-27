f8c7ca15e97f29efc0ba22fe6038a82dc8f29f2b
Tim Oxley <secoif@gmail.com>
2011-08-27 13:00:16 +1000

    Basic server with jade/stylus templating

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
-lib/server.js
+(function() {
+  var app, express, fs, http, nib, nko, path, stylus;
+  http = require('http');
+  nko = require('nko')('ahE2gHoOKLxdrUI0');
+  express = require('express');
+  fs = require('fs');
+  path = require('path');
+  stylus = require('stylus');
+  nib = require('nib');
+  app = express.createServer();
+  app.configure(function() {
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(stylus.middleware({
+      src: "" + __dirname + "/app/views",
+      dest: "" + __dirname + "/public",
+      force: true,
+      compile: function(str, path) {
+        return stylus(str).set('filename', path).set('compress', false).set('warn', true).use(nib());
+      }
+    }));
+    app.set('views', "" + __dirname + "/app/views");
+    app.set('view engine', 'jade');
+    app.get('/', function(req, res) {
+      var options;
+      options = {};
+      options.locals = {
+        project: "Applied Phlebotinum",
+        page_title: "Home"
+      };
+      return res.render('index', options);
+    });
+    app.use(express.logger());
+    return app.use(express.static("" + __dirname + "/public"));
+  });
+  app.listen(parseInt(process.env.PORT) || 7777);
+  console.log('Listening on ' + app.address().port);
+}).call(this);
--- src/server.coffee
+++ src/server.coffee
@@ -1,10 +1,44 @@
 http = require('http')
 nko = require('nko')('ahE2gHoOKLxdrUI0')
 
-app = http.createServer (req, res) ->
-	res.writeHead 200
-		'Content-Type': 'text/html'
-	res.end 'Hello, World'
+express   = require('express')
+fs        = require('fs')
+path      = require('path')
+
+# view libs
+stylus    = require('stylus')
+nib       = require('nib')
+
+
+
+# Express configuration
+app = express.createServer()
+app.configure ->
+  app.use express.bodyParser()
+  app.use express.cookieParser()
+  app.use stylus.middleware
+    src: "#{__dirname}/app/views"
+    dest:     "#{__dirname}/public"
+    force:   true
+    compile: (str, path) ->
+      return stylus(str)
+        .set('filename', path)
+        .set('compress', false)
+        .set('warn', true)
+        .use(nib())
+
+  app.set 'views', "#{__dirname}/app/views"
+  app.set 'view engine', 'jade'
+
+  app.get '/', (req, res) ->
+    options = {}
+    options.locals =
+      project: "Applied Phlebotinum"
+      page_title: "Home"
+    res.render 'index', options
+
+  app.use express.logger()
+  app.use express.static "#{__dirname}/public"
 
 app.listen parseInt(process.env.PORT) || 7777
 console.log 'Listening on ' + app.address().port
